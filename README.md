0e70b674bb43db0e157b40e2f9702896948e6439
Rafael de Oliveira <rafael@oliveira.dev>
2020-08-25 17:15:17 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# About
+This plugins emits a file named `[assetName].hash` containing assets hash.
+
+Currently it looks for the query parameter `v` in the asset name.
+
+
+# Usage
+
+`yarn add -D webpack-file-hash-plugin`
+
+```js
+const WebpackFileHashPlugin = require('webpack-file-hash-plugin');
+
+module.exports {
+    output: {
+        filename: '[name].js?v=[hash]',
+        chunkFilename: '[id].js?v=[contenthash]',
+    },
+    plugins: [
+        new WebpackFileHashPlugin()
+    ]
+}
+```
+
+Example output files:
+- myEntry.js
+- myEntry.js.hash 
+- otherEntry.js
+- otherEntry.js.hash 
+
+
+# Options 
+
+|Name|Description|Default|
+|-----|----------|-----|
+|`paramName`|Parameter containing tha hash|`v`|
+|`hashFileExtension`|Extension of the generated hash file|`hash`|
+|`extractHashFromAssetName(assetName, options)`|Function to extract the hash from asset name| -|
+
+
+
---
+++ index.js
@@ -0,0 +1,92 @@
+const { RawSource } = require('webpack-sources');
+const validateOptions = require('schema-utils');
+
+const schema = {
+    type: 'object',
+    properties: {
+        paramName: {
+            type: 'string'
+        },
+        hashFileExtension: {
+            type: 'string'
+        },
+        extractHashFromAssetName: {
+            instanceof: "Function"
+        }
+    }
+};
+
+const defaultOptions = {
+    paramName: 'v',
+    hashFileExtension: 'hash',
+    extractHashFromAssetName(name, options) {
+        const params = extractQueryParams(name);
+        return params[options.paramName] || null;
+    }
+};
+
+function extractQueryParams(uri) {
+    const paramsIdx = uri.indexOf('?');
+    if (paramsIdx === -1) {
+        return null;
+    }
+    const params = {};
+    uri.substr(paramsIdx + 1).split('&').forEach(p => {
+        const [key, value] = p.split('=');
+        params[key] = value || '';
+    });
+    return params;
+}
+
+
+
+
+class WebpackHashFilePlugin {
+
+    constructor(options = {}) {
+        validateOptions(schema, options, {name: 'HashFilePlugin'});
+        this.options = {...defaultOptions, ...options};
+    }
+
+
+    generateHashFileName(name) {
+        const queryIdx = name.indexOf('?');
+        let newName = name;
+        if (queryIdx >= 0) {
+            newName = newName.substr(0, queryIdx);
+        }
+        return newName + '.' + this.options.hashFileExtension;
+    }
+
+    apply(compiler) {
+
+        compiler.hooks.emit.tap('WebpackHashFilePlugin', (
+            compilation, callback
+        ) => {
+            let hashes = {};
+            for (let asset in compilation.assets) {
+                if (compilation.assets.hasOwnProperty(asset)) {
+                    const hash = this.options.extractHashFromAssetName(asset, this.options);
+                    if (hash !== null) {
+                        if (!(typeof hash === 'string')) {
+                            compilation.errors.push(new Error('Hash must be a string.'))
+                            continue;
+                        }
+                        const hashFileName = this.generateHashFileName(asset);
+                        hashes[hashFileName] = new RawSource(hash);
+                    }
+                }
+            }
+
+            for (let asset in hashes) {
+                compilation.assets[asset] = hashes[asset];
+            }
+
+            if (typeof callback === 'function') {
+                callback();
+            }
+        });
+    }
+}
+
+module.exports = WebpackHashFilePlugin;
